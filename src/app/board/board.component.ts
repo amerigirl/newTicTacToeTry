ba108cbe0382c4405cfec5d75913b8103501ed03
Be-Asia McKerracher <amerigirl29@gmail.com>
2023-01-22 11:41:52 -0600

    slowling learning/building

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-board',
@@ -15,10 +15,25 @@ import { Component } from '@angular/core';
 export class BoardComponent {
   squares: string[] = [];      //you have to initialize--set to a string initially
   xIsNext: boolean = false; //you have to initialize!!
-  winner: string = '';  //initialized to an empty string for now
+  winner?: string;  //initialized to an empty string for now
 
-  constructor(){
+  constructor(){}
 
+  ngOnInit(){
+    this.newGame();
   }
+
+  newGame(){
+    this.squares = Array(9).fill(null); //not 100% here
+    this.winner = '';
+    this.xIsNext = true;
+  }
+
+  getPlayer(){
+    return this.xIsNext ? 'O' : 'X';  //read conditionals in typescript aloud for practice
+  }
+
+
+
 
 }
